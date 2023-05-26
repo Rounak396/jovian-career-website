e3725fc1794d68a8452e76a9c2d340e2d8a08e93
TheCatalog360 <98696211+TheCatalog360@users.noreply.github.com>
2023-05-26 18:29:10 +0000

    Updated the HTML and Flask files

--- app.py
+++ app.py
@@ -2,9 +2,40 @@ from flask import Flask, render_template
 
 app= Flask(__name__)
 
+JOBS= [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary':'Rs. 16,00,000'
+  },
+
+  {
+    'id': 2,
+    'title': 'Frontend Developer',
+    'location': 'San Fransisco, USA',
+    'salary': '$ 1,20,000',
+  },
+
+  {
+    'id': 3,
+    'title': 'Backend Developer',
+    'location': 'New Delhi, India',
+    'salary': 'Rs. 12,00,000',
+  },
+
+  {
+    'id': 4,
+    'title': 'Android Developer',
+    'location': 'Remote',
+    'salary': 'Rs. 15,00,000'
+  }
+]
+
 @app.route("/")
 def hello_jovian():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, 
+                                company_name= 'Jovian' )
 
 if __name__=="__main__":
   app.run(host='0.0.0.0', debug=True)
